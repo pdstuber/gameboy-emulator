318e379f0aa223dd03366e6564e0c30809b53a4e
Philipp Stuber <stuberphd@gmail.com>
2024-01-13 00:25:02 +0100

    current state

--- go.mod
+++ go.mod
@@ -3,11 +3,19 @@ module github.com/pdstuber/gameboy-emulator
 go 1.21.3
 
 require (
+	github.com/hajimehoshi/ebiten/v2 v2.6.3
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.8.0
 )
 
 require (
+	github.com/ebitengine/purego v0.5.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jezek/xgb v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.12.0 // indirect
+	golang.org/x/mobile v0.0.0-20230922142353-e2f452493d57 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
--- internal/ppu/ppu.go
+++ internal/ppu/ppu.go
@@ -3,31 +3,58 @@ package ppu
 import (
 	"context"
 	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
 )
+
+type Color uint8
+
+const (
+	White Color = iota
+	LightGrey
+	DarkGrey
+	Black
+)
+
+type Tile [8][8]Color
 
 type PPU struct {
 	cyclesChannel chan int
 	errorChannel  chan error
+	pixels        []byte
+	background    [][]Tile
+	screenWidth   int
+	screenHeight  int
 }
 
 func New() *PPU {
+	b := make([][]Tile, 20)
+	for i := range b {
+		b[i] = make([]Tile, 18)
+	}
 	return &PPU{
 		cyclesChannel: make(chan int),
 		errorChannel:  make(chan error),
+		background:    b,
+		pixels:        make([]byte, 160*144*4),
+		screenWidth:   160,
+		screenHeight:  144,
 	}
 }
 
-func (ppu *PPU) Start(ctx context.Context) error {
+func (p *PPU) Start(ctx context.Context) error {
 	log.Println("starting ppu")
 	go func() {
-		for {
-			cycles := <-ppu.cyclesChannel
-			log.Printf("elapsed cycles: %d\n", cycles)
+		ebiten.SetWindowSize(p.screenWidth*6, p.screenHeight*6)
+		ebiten.SetWindowTitle("Gameboy Emulator")
+		if err := ebiten.RunGame(p); err != nil {
+			log.Fatal(err)
 		}
+
 	}()
 
 	select {
-	case err := <-ppu.errorChannel:
+	case err := <-p.errorChannel:
 		return err
 	case <-ctx.Done():
 		return nil
@@ -38,4 +65,17 @@ func (p *PPU) NotifyCycles(cycles int) {
 	go func() {
 		p.cyclesChannel <- cycles
 	}()
+}
+
+func (p *PPU) Update() error {
+	return nil
+}
+
+// http://www.codeslinger.co.uk/pages/projects/gameboy/graphics.html
+func (p *PPU) Draw(screen *ebiten.Image) {
+	screen.WritePixels(p.pixels)
+}
+
+func (p *PPU) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return p.screenWidth, p.screenHeight
 }
